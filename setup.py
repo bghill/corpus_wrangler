8b80e0ab1f7ada1822ecbea2489216d249e7c430
Brandon Hill <bghill.dev@gmail.com>
2020-11-04 12:16:33 -0800

    Updates project to drop support for Python 3.6 and add 3.9.  Previous check-in used importlib's resource module which is a Python 3.7 and higher feature.

--- setup.py
+++ setup.py
@@ -19,16 +19,16 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Brandon Hill",
     author_email='bghill.dev@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="A simple library for downloading and handling large corpora (like Wikipedia).",
     install_requires=requirements,
